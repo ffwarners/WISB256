7107f3cae753ea9100e30436b70d86286c06f52e
ffwarners <ffwarners@gmail.com>
2015-04-21 10:23:33 +0200

    Versie 1

---
+++ Opdracht1/ManyGreetings.py
@@ -0,0 +1,17 @@
+def askname():
+    Name = input("Name? ")
+    print('Hello ', Name)
+
+def greeting():
+    Name = input("Name? ")
+    N_greeting = input("How many greetings? ")
+    print(N_greeting,"x",Name)
+
+def greeting2():
+    Name = input("What is your name? ")
+    N_greeting = input("How many greetings? ")
+    print(N_greeting,"x Hello,",Name)
+
+askname()
+
+
